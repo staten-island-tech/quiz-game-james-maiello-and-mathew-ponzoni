48ad7db85016ec7ed090342c4f51563a5240750a
JamesMaiello <james.maiello23@sitechhs.com>
2021-04-07 17:33:01 -0400

    f

--- styles/app.js
+++ styles/app.js
@@ -1,11 +1,11 @@
-var questions = {
+const questions = {
      new Question("Which state has the highest population?", ["New York", "Texas", "California", "Florida"], "California"),
      new Question("Which state has the largest Land Mass?", ["Alaska", "California", "New York", "Montana"], "Alaska"),
      new Question("Whcih state has the lowest population?", ["Alaska", "Maine", "Wyoming", "South Dakota"], "Wyoming"),
     
 }
 
-var quiz = new Quiz(questions);
+const quiz = new Quiz(questions);
 
 populate();
 
@@ -14,7 +14,7 @@ function populate() {
 
     }
     else{
-        var element = document.getElementById("question");
+        const element = document.getElementById("question");
         element.innerHTML = quiz.getQuestionIndex().text;
     }
 }
