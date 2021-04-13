7f61159670bbc45a946ee4985e4e808464cac100
JamesMaiello <james.maiello23@sitechhs.com>
2021-04-12 20:13:54 -0400

    Finished

--- styles/index.js
+++ styles/index.js
@@ -3,6 +3,7 @@ const choices = Array.from(document.getElementsByClassName('choice-text'));
 const questionCounterText = document.getElementById('questionCounter');
 const scoreText = document.getElementById('score');
 
+
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
@@ -48,7 +49,8 @@ startGame = () => {
 
 getNewQuestion = () => {
     if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
-        return window.location.assign('/end.html');
+        localStorage.setItem("mostRecentScore", score);
+        return window.location.assign('end.html');
     }
     questionCounter++;
     questionCounterText.innerText = questionCounter + "/" + MAX_QUESTIONS;
@@ -93,3 +95,4 @@ incrementScore = num => {
  scoreText.innerText = score;
 }
 startGame();
+
